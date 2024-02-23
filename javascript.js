e18393a28db62091967a27ffaf470f23ffd6c470
M sufyan <sufyanmalikbhiriaroad1234@gmail.com>
2024-02-23 09:46:55 -0800

    js

---
+++ javascript.js
@@ -0,0 +1,55 @@
+// var number = prompt('give me any nuber')
+// var addOrEven = number %2;
+// alert (addOrEven == 0)
+
+// var message = 'Thanks'
+// var useName = "susan"
+// var banger = '!'
+// var customMess = message+useName+banger
+// alert(customMess)
+
+var number = prompt('give me any number');
+
+var reminder = number % 2 ;
+
+var value = reminder == 0;
+
+var message = ' ';
+
+
+if (value){
+    message ='number is even'
+
+    
+}
+else {
+    message ='number is odd'
+}
+alert(
+    message
+)
+
+
+
+
+
+
+
+// var number = prompt ('give me any number')
+
+// var reminder = number % 2;
+
+// var value = reminder == 0;
+
+// var message = '';
+
+// if (value){
+//     message = "number is even"
+   
+// }
+// else{
+//     message = "number is odd"
+// }
+// alert(
+//     message
+// )
